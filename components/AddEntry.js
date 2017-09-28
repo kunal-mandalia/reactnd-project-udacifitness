451e8a52340b5e461723ad9317ec749d91eb9c7f
kunal-mandalia <kunal.v.mandalia@gmail.com>
2017-09-28 23:20:20 +0100

    feat: add increment/decrement  components for AddEntry

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,12 +1,70 @@
 import React, { Component } from 'react'
 import { View } from 'react-native'
 import { getMetricMetaInfo } from '../utils/helpers'
+import UdacitySlider from './UdacitySlider'
+import UdacitySteppers from './UdacitySteppers'
+import DateHeader from './DateHeader'
 
 export default class AddEntry extends Component {
+  state = {
+    run: 0,
+    bike: 0,
+    swim: 0,
+    sleep: 0,
+    eat: 0
+  }
+
+  increment = metric => {
+    const { max, step } = getMetricMetaInfo(metric)
+
+    this.setState(state => {
+      const count = state[metric] + step
+      return {
+        ...state,
+        [metric]: count > max ? max : count
+      }
+    })
+  }
+
+  decrement = metric => {
+    const { step } = getMetricMetaInfo(metric)
+
+    this.setState(state => {
+      const count = state[metric] - step
+      return {
+        ...state,
+        [metric]: count < 0 ? 0 : count
+      }
+    })
+  }
+
+  slide = (metric, value) => {
+    this.setState({
+      [metric]: value
+    })
+  }
+
   render() {
+    const metaInfo = getMetricMetaInfo()
+
     return (
       <View>
-        {getMetricMetaInfo('bike').getIcon()}
+        <DateHeader date={(new Date().toLocaleDateString())} />
+        {Object.keys(metaInfo).map(key => {
+          const { getIcon, type, ...rest } = metaInfo[key]
+          const value = this.state[key]
+          
+          return (
+            <View key={key}>
+              {getIcon()}
+
+              {type === 'slider'
+                ? <UdacitySlider />
+                : <UdacitySteppers />
+              }
+            </View>
+          )
+        })}
       </View>
     )
   }
