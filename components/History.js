26832567f3352587f94004527dc45f92026936ba
kunal-mandalia <kunal.v.mandalia@gmail.com>
2017-10-07 23:22:49 +0100

    feat: add AppLoading

--- components/History.js
+++ components/History.js
@@ -8,8 +8,13 @@ import UdacifitnesssCalendar from 'udacifitness-calendar'
 import { white } from '../utils/colors'
 import DateHeader from './DateHeader'
 import MetricCard from './MetricCard'
+import { AppLoading } from 'expo'
 
 class History extends Component {
+  state = {
+    ready: false
+  }
+
   componentDidMount () {
     const { dispatch } = this.props
 
@@ -22,6 +27,7 @@ class History extends Component {
           }))
         }
       })
+      .then(() => this.setState(() => ({ ready: true })))
   }
 
   renderItem = ({ today, ...metrics }, formattedDate, key) => (
@@ -50,6 +56,12 @@ class History extends Component {
 
   render () {
     const { entries } = this.props
+    const { ready } = this.state
+
+    if (ready === false) {
+      return <AppLoading />
+    }
+
     return (
       <UdacifitnesssCalendar
         items={entries}
