93232e46dfbed5c42c07e34aad0489363f52668a
kunal-mandalia <kunal.v.mandalia@gmail.com>
2017-10-08 13:45:33 +0100

    feat: add tab styling

--- App.js
+++ App.js
@@ -31,7 +31,17 @@ const Tabs = TabNavigator({
     },
     tabBarOptions: {
       activeTintColor: Platform.OS === 'ios' ? purple : white,
-      color: Platform.OS === 'ios' ? white : purple
+      style: {
+        height: 56,
+        backgroundColor: Platform.OS === 'ios' ? white : purple,
+        shadowColor: 'rgba(0, 0, 0, 0.24)',
+        shadowOffset: {
+          width: 0,
+          height: 3,
+        },
+        shadowRadius: 6,
+        shadowOpacity: 1
+      }
     }
   }
 )
